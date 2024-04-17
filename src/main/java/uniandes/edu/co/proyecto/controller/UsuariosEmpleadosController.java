a55f9e92090d75fa2c62675be31a51a60482931a
Jairo <ja.fierro@uniandes.edu.co>
2024-04-16 23:58:59 -0500

    Avance

--- src/main/java/uniandes/edu/co/proyecto/controller/UsuariosEmpleadosController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/UsuariosEmpleadosController.java
@@ -25,6 +25,37 @@ public class UsuariosEmpleadosController {
         return "usuariosEmpleados";
     }
 
+    @GetMapping("/usuariosEmpleados/{login}/{password_empleado}")
+    public String usuariosEmpleadosCargo(@PathVariable("login") String login, @PathVariable("password_empleado") String password_empleado, Model model) {
+        
+        String paginaDestino = "index";
+        
+        if(login != null && !login.equals("") && password_empleado != null && !password_empleado.equals(""))
+        { 
+            System.out.println("entró");
+            System.out.println(login);
+            System.out.println(password_empleado);
+            String cargo= usuarioEmpleadoRepository.verificarUsuarioEmpleadoYObtenerCargo(login, password_empleado);
+            System.out.println(cargo);
+            
+            if (cargo.equals("Gerente oficina")) {
+                paginaDestino = "gerenteOficina";
+            } else if (cargo.equals("Gerente general")) {
+                paginaDestino = "gerenteGeneral";
+            } else if (cargo.equals("Cajero")) {
+                paginaDestino = "cajero";
+            }else if (cargo.equals("Administrador")) {
+                paginaDestino = "administrador";
+            }
+
+        } else {
+            
+            paginaDestino = "loginIncorrecto";
+        }
+
+        return paginaDestino;
+    }
+
     @GetMapping("/usuariosEmpleados/new")
     public String usuarioEmpleadoForm(Model model) {
         model.addAttribute("usuarioEmpleado", new UsuarioEmpleado());
@@ -35,7 +66,7 @@ public class UsuariosEmpleadosController {
     public String usuarioEmpleadoSave(@ModelAttribute UsuarioEmpleado usuarioEmpleado) {
 
         usuarioEmpleadoRepository.insertarUsuarioEmpleado(usuarioEmpleado.getLogin(), usuarioEmpleado.getPassword_empleados(), 
-                                                        usuarioEmpleado.getId());
+                                                          usuarioEmpleado.getId());
         
         return "redirect:/usuariosEmpleados";
     }
