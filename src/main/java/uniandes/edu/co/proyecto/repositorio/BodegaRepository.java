b1d9e4c7b874148c412555a766da6e3e8cba79d9
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-18 02:29:48 -0500

    carpetaRepositorio

---
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/BodegaRepository.java
@@ -0,0 +1,9 @@
+package uniandes.edu.co.proyecto.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uniandes.edu.co.proyecto.modelo.Bodega;
+
+public interface BodegaRepository extends JpaRepository<Bodega, Integer>{
+    
+}
