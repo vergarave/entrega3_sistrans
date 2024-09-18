e63c12d84cc7911eda35522ee376a95750ee56be
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-18 00:00:43 -0500

    atributosBodega

--- src/main/java/uniandes/edu/co/proyecto/modelo/Bodega.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Bodega.java
@@ -1,10 +1,22 @@
 package uniandes.edu.co.proyecto.modelo;
 
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="bodega")
 public class Bodega {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+
+    private Integer id;
+    private String nombre;
+    private Double tamanio;
+    private Double porcentajeOcupacion;
+    private Integer capacidad;
+    private Integer idSucursal;
 }
