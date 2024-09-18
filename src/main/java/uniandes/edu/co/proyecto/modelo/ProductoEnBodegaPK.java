1727964f4a839aa204c1631ba2e691bfe6224347
Vanesa Montaño <k.montano@uniandes.edu.co>
2024-09-18 00:56:18 -0500

    no se subieron las entidades en el anterior

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ProductoEnBodegaPK.java
@@ -0,0 +1,46 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class ProductoEnBodegaPK implements Serializable{
+    
+    @ManyToOne
+    @JoinColumn(name="identificador_Producto", referencedColumnName="identificador")
+    private Producto idProducto;
+    
+    @ManyToOne
+    @JoinColumn(name="id_Bodega", referencedColumnName="id")
+    private Bodega idBodega;
+
+    public ProductoEnBodegaPK(Producto idProducto, Bodega idBodega) {
+        super();
+        this.idProducto = idProducto;
+        this.idBodega = idBodega;
+    }
+
+    public Producto getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Producto idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Bodega getIdBodega() {
+        return idBodega;
+    }
+
+    public void setIdBodega(Bodega idBodega) {
+        this.idBodega = idBodega;
+    }
+
+    
+
+    
+
+}
--- src/main/java/uniandes/edu/co/proyecto/modelo/Proveedor.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Proveedor.java
@@ -1,5 +1,75 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="proveedor")
 public class Proveedor {
+
+    private String nit;
+    private String nombre;
+    private String direccion;
+    private String nombrePersonaContacto;
+    private String telefonoPersonaContacto;
+
+    public Proveedor(String nit, String nombre, String direccion, 
+                        String nombrePersonaContacto, 
+                        String telefonoPersonaContacto)
+    {   
+        this.nit = nit;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.nombrePersonaContacto = nombrePersonaContacto;
+        this.telefonoPersonaContacto = telefonoPersonaContacto;
+    }
+
+    public Proveedor()
+    {;}
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombrePersonaContacto() {
+        return nombrePersonaContacto;
+    }
+
+    public void setNombrePersonaContacto(String nombrePersonaContacto) {
+        this.nombrePersonaContacto = nombrePersonaContacto;
+    }
+
+    public String getTelefonoPersonaContacto() {
+        return telefonoPersonaContacto;
+    }
+
+    public void setTelefonoPersonaContacto(String telefonoPersonaContacto) {
+        this.telefonoPersonaContacto = telefonoPersonaContacto;
+    }
+
+    
     
 }
+
