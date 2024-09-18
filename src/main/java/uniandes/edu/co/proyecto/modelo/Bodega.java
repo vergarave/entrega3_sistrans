b9daa50fe10fea6a401662d7e7d7f5c1ec5d80bc
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-18 00:19:00 -0500

    entidad Bodega

--- src/main/java/uniandes/edu/co/proyecto/modelo/Bodega.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Bodega.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,5 +20,69 @@ public class Bodega {
     private Double tamanio;
     private Double porcentajeOcupacion;
     private Integer capacidad;
-    private Integer idSucursal;
+
+    @ManyToOne
+    @JoinColumn(name="id_sucursal", referencedColumnName="id")
+    private Sucursal idSucursal;
+
+    public Bodega(String nombre, Double tamanio, Double porcentajeOcupacion, Integer capacidad, Sucursal idSucursal){
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+        this.porcentajeOcupacion = porcentajeOcupacion;
+        this.capacidad = capacidad;
+        this.idSucursal = idSucursal;
+    }
+
+    public Bodega(){
+    ;}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Double getTamanio() {
+        return tamanio;
+    }
+
+    public Double getPorcentajeOcupacion() {
+        return porcentajeOcupacion;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public Sucursal getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTamanio(Double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setPorcentajeOcupacion(Double porcentajeOcupacion) {
+        this.porcentajeOcupacion = porcentajeOcupacion;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public void setIdSucursal(Sucursal idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    
 }
