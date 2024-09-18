844bcc1690d4f0486616221fb9a39897856837d3
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-18 03:36:37 -0500

    tamanio

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,13 +1,29 @@
 package uniandes.edu.co.proyecto;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProyectoApplication {
+import uniandes.edu.co.proyecto.modelo.Bodega;
+import uniandes.edu.co.proyecto.repositorio.BodegaRepository;
 
+@SpringBootApplication
+public class ProyectoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private BodegaRepository bodegaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
 
+	@Override
+	public void run(String...arg){
+		Collection<Bodega> bodegas = bodegaRepository.darBodegas();
+		for(Bodega b: bodegas){
+			System.out.println(b);
+		}
+	}
 }
--- src/main/java/uniandes/edu/co/proyecto/repositorio/BodegaRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/BodegaRepository.java
@@ -1,9 +1,30 @@
 package uniandes.edu.co.proyecto.repositorio;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.Bodega;
 
 public interface BodegaRepository extends JpaRepository<Bodega, Integer>{
     
+    @Query(value = "SELECT * FROM bodega", nativeQuery=true)
+    Collection<Bodega> darBodegas();
+
+    @Query(value = "SELECT * FROM bodega WHERE id= :id", nativeQuery=true)
+    Bodega darBodega(@Param("id") int id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO bodega(id, nombre, tamanio, porcentaje_ocupacion, capacidad, id_sucursal) VALUES(secuencia_bodega.nextval, :nombre, :tamanio, :porcentajeOcupacion, :capacidad, :idSucursal)", nativeQuery = true)
+    void insertarBodega(@Param("nombre") String nombre, @Param("tamanio") Double tamanio, @Param("porcentajeOcupacion") Double porcentajeOcupacion, @Param("capacidad") Integer capacidad, @Param("idSucursal") Integer idSucursal);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE from bodega WHERE id = :id", nativeQuery= true)
+    void eliminarBodega(@Param("id") int id);
 }
