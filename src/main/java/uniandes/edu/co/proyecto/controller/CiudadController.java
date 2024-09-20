9ffc091e19b3a71baa3fbd0090122d8502f6c7e3
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-20 16:27:02 -0500

    ciudadController

--- src/main/java/uniandes/edu/co/proyecto/controller/CiudadController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/CiudadController.java
@@ -1,8 +1,41 @@
 package uniandes.edu.co.proyecto.controller;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import uniandes.edu.co.proyecto.modelo.Ciudad;
+import uniandes.edu.co.proyecto.repositorio.CiudadRepository;
+
 
 @RestController
 public class CiudadController {
 
+    @Autowired
+    private CiudadRepository ciudadRepository;
+
+    @GetMapping("/ciudades")
+    public Collection<Ciudad> ciudades(){
+        return ciudadRepository.darCiudades();
+    }
+
+    @PostMapping("/ciudades/new/save")
+    public ResponseEntity<String> ciudadGuardar(@RequestBody Ciudad ciudad){
+        
+        try{
+            ciudadRepository.insertarCiudad(ciudad.getNombre());
+            return new ResponseEntity<>("Ciudad creada exitosamente", HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>("Error al crear la ciudad", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    
 }
