1a665137379ab80082f827f56e653d933db8dee9
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-20 20:21:46 -0500

    deleteMappingFinal

--- src/main/java/uniandes/edu/co/proyecto/controller/BodegaController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/BodegaController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,7 +53,7 @@ public class BodegaController {
         }
     }
 
-    @GetMapping("/bodegas/{id}/delete")
+    @DeleteMapping("/bodegas/{id}/delete")
     public ResponseEntity<String> bodegaEliminar(@PathVariable("id") int id) {
 
         bodegaRepository.eliminarBodega(id);
