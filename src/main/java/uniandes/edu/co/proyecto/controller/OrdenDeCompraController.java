c3666219ebf156901cefcb812e70014ed914825b
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2024-09-28 04:41:04 -0500

    djjdjdjd

--- src/main/java/uniandes/edu/co/proyecto/controller/OrdenDeCompraController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/OrdenDeCompraController.java
@@ -29,7 +29,7 @@ public class OrdenDeCompraController {
         try {
             ordenDeCompraRepository.insertarOrdenDeCompra(
                 ordenDeCompra.getFechaEntrega(),
-                "vigente",
+                "VIGENTE",
                 LocalDate.now(),
                 ordenDeCompra.getIdSucursal(),
                 ordenDeCompra.getNitProveedor()
