2aaf6bca588a439fe5b887c7a9cb0d726c81708e
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-09-28 03:12:25 -0500

    cambios en Producto Repository

--- src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoRepository.java
@@ -1,8 +1,8 @@
 package uniandes.edu.co.proyecto.repositorio;
 
 
-import java.util.Collection;
 import java.time.LocalDate;
+import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -23,15 +23,15 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO producto( identificador, nombre, costoEnBodega, presentacion, cantidadPresentacion, unidadMedida, volumenEmpaque, pesoEmpaque, fechaExpiracion, codigoDeBarras, clasificacionCategoria) VALUES(secuencia_producto.nextval, :nombre, :costoEnBodega, :presentacion, :cantidadPresentacion, :unidadMedida, :volumenEmpaque, :pesoEmpaque, :fechaExpiracion, :codigoDeBarras, :clasificacionCategoria)", nativeQuery = true)
+    @Query(value = "INSERT INTO producto( identificador, nombre, costo_en_bodega, presentacion, cantidad_presentacion, unidad_medida, volumen_empaque, peso_empaque, fecha_expiracion, codigo_de_barras, clasificacion_categoria) VALUES(secuencia_producto.nextval, :nombre, :costo_en_bodega, :presentacion, :cantidad_presentacion, :unidad_medida, :volumen_empaque, :peso_empaque, :fecha_expiracion, :codigo_de_barras, :clasificacion_categoria)", nativeQuery = true)
     void insertarProducto(@Param("nombre") String nombre, @Param("costo_en_bodega") Double costoEnBodega, @Param("presentacion") String presentacion, @Param("cantidad_presentacion") Double cantidadPresentacion, @Param("unidad_medida") String unidadMedida, 
                           @Param("volumen_empaque") String volumenEmpaque, @Param("peso_empaque") String pesoEmpaque, @Param("fecha_expiracion")LocalDate fechaExpiracion, @Param("codigo_de_barras") String codigoDeBarras, @Param("clasificacion_categoria") Categoria clasificacionCategoria);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE producto SET nombre = :nombre, costoEnBodega = :costoEnBodega, presentacion = :presentacion,cantidadPresentacion = :cantidadPresentacion,unidadMedida = :unidadMedida,volumenEmpaque = :volumenEmpaque, pesoEmpaque = :pesoEmpaque, fechaExpiracion = :fechaExpiracion, codigoDeBarras = :codigoDeBarras, clasificacionCategoria = :clasificacionCategoria WHERE identificador = :identificador", nativeQuery = true)
-    void actualizarProducto(@Param("identificador")Integer identificador, @Param("nombre") String nombre, @Param("costoEnBodega") Double costoEnBodega, @Param("presentacion") String presentacion, @Param("cantidadPresentacion") Double cantidadPresentacion, 
-                          @Param("unidadMedida") String unidadMedida, @Param("volumenEmpaque") String volumenEmpaque, @Param("pesoEmpaque") String pesoEmpaque, @Param("fechaExpiracion")LocalDate fechaExpiracion, @Param("codigoDeBarras") String codigoDeBarras, @Param("clasificacion_categoria") Categoria clasificacionCategoria);                      
+    @Query(value = "UPDATE producto SET nombre = :nombre, costo_en_bodega = :costo_en_bodega, presentacion = :presentacion,cantidad_presentacion = :cantidad_presentacion,unidad_medida = :unidad_medida,volumen_empaque = :volumen_empaque, peso_empaque = :peso_empaque, fecha_expiracion = :fecha_expiracion, codigo_de_barras = :codigo_de_barras, clasificacion_categoria = :clasificacion_categoria WHERE identificador = :identificador", nativeQuery = true)
+    void actualizarProducto(@Param("identificador")Integer identificador, @Param("nombre") String nombre, @Param("costo_en_bodega") Double costoEnBodega, @Param("presentacion") String presentacion, @Param("cantidad_presentacion") Double cantidadPresentacion, 
+                          @Param("unidad_medida") String unidadMedida, @Param("volumen_empaque") String volumenEmpaque, @Param("peso_empaque") String pesoEmpaque, @Param("fecha_expiracion")LocalDate fechaExpiracion, @Param("codigo_de_barras") String codigoDeBarras, @Param("clasificacion_categoria") Categoria clasificacionCategoria);                      
     @Modifying
     @Transactional
     @Query(value = "DELETE from producto WHERE identificador = :identificador", nativeQuery= true)
