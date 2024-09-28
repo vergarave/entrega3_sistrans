81717443b36740bb05d60b57f9d9cb279f9c8e5c
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2024-09-28 03:22:52 -0500

    siwi

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,16 +1,31 @@
 package uniandes.edu.co.proyecto;
 
 
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import uniandes.edu.co.proyecto.modelo.Producto;
+import uniandes.edu.co.proyecto.repositorio.ProductoRepository;
+
 
 @SpringBootApplication
-public class ProyectoApplication{
+public class ProyectoApplication implements CommandLineRunner{
 	
+	@Autowired
+	private ProductoRepository productoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	public void run(String...arg) {
+		Collection<Producto> productos = productoRepository.darProductos();
+		for (Producto p: productos) {
+			System.out.println(p);
+		}
 	}
 
 }
