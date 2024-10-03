8e081cce1a8f084f5c9fe6eba4e0481ad61d8aeb
Juan Felipe Hernández <jf.hernandezp1@uniandes.edu.co>
2024-10-02 19:19:07 -0500

    Correccrion PK repositories

--- src/main/java/uniandes/edu/co/proyecto/repositorio/CompraRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/CompraRepository.java
@@ -1,5 +1,10 @@
 package uniandes.edu.co.proyecto.repositorio;
 
-public interface CompraRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uniandes.edu.co.proyecto.modelo.Compra;
+import uniandes.edu.co.proyecto.modelo.CompraPK;
+
+public interface CompraRepository extends JpaRepository<Compra,CompraPK>{
 
 }
