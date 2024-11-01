aa92f68ca8274f7adeedee00951bdff7f0a97a23
Vanesa Montaño <k.montano@uniandes.edu.co>
2024-11-01 17:32:00 -0500

    RF5 - Crear y leer una Categoría

--- src/main/java/uniandes/edu/co/proyecto/controller/CategoriaController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/CategoriaController.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import uniandes.edu.co.proyecto.modelo.Categoria;
 import uniandes.edu.co.proyecto.repositorio.CategoriaRepository;
@@ -86,12 +86,17 @@ public class CategoriaController {
     //Metodos nuevos para le HTML
     @GetMapping("/menuCategoria")
     public String menuCategoria() {
-        return "menuCategoria";
+        return "menuCategoria"; //Nombre del archivo HTML
     }
 
     @GetMapping("/categorias/nueva")
     public String mostrarFormularioNuevaCategoria() {
-        return "nuevaCategoria"; // Este nombre debe coincidir con el archivo HTML en la carpeta de plantillas
+        return "nuevaCategoria"; //Nombre del archivo HTML
+    }
+
+    @GetMapping("/categorias/buscar")
+    public String mostrarFormularioBuscarCategoria() {
+        return "buscarCategoria"; //Nombre del archivo HTML
     }
 
 }
--- src/main/java/uniandes/edu/co/proyecto/controller/ProveedorController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/ProveedorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import uniandes.edu.co.proyecto.modelo.Proveedor;
 import uniandes.edu.co.proyecto.repositorio.ProveedorRepository;
