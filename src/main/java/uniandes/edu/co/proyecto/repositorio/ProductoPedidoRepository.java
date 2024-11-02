159f9501fa7e2fcbe2392cf0792c77cf18a44cf9
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2024-11-01 19:17:15 -0500

    reducirMetodo

--- src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoPedidoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ProductoPedidoRepository.java
@@ -6,22 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import uniandes.edu.co.proyecto.modelo.Producto;
 import uniandes.edu.co.proyecto.modelo.ProductoPedido;
 import uniandes.edu.co.proyecto.modelo.ProductoPedidoPK;
 
 public interface ProductoPedidoRepository extends JpaRepository<ProductoPedido, ProductoPedidoPK> {
-
-    //NEW NEW NEW 
-    //Esta consulta es para obtener todos los productos relacionados con una orden de compra específica
-    //Básicamente hace un join entre las tablas PRODUCTO y PRODUCTO_PEDIDO 
-    //Luego filtra los resultados para que solo incluya productos de la orden de compra que se nos dio
-    @Query(value = "SELECT * \r\n" +
-                   "FROM PRODUCTO \r\n" +
-                   "INNER JOIN PRODUCTO_PEDIDO ON PRODUCTO.IDENTIFICADOR = PRODUCTO_PEDIDO.IDENTIFICADOR_PRODUCTO \r\n" +
-                   "WHERE PRODUCTO_PEDIDO.NUMERO_ORDEN_DE_COMPRA = :numeroOrdenCompra", 
-           nativeQuery = true)
-    Collection<Producto> obtenerProductosPorOrdenDeCompra(@Param("numeroOrdenCompra") Integer numeroOrdenCompra);
 
     //NEW NEW NEW 
     //Aquí vemos la tabla PRODUCTO_PEDIDO para obtener los productos y cantidades asociados a una orden de compra específica
