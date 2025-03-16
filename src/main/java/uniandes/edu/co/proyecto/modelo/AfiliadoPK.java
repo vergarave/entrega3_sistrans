9644d0efb53637cc78039d3a29bfa1198c392604
Andrea Davila <a.davilab2@uniandes.edu.co>
2025-03-16 12:15:13 -0500

    commit para act el error :(

--- src/main/java/uniandes/edu/co/proyecto/modelo/AfiliadoPK.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/AfiliadoPK.java
@@ -1,20 +1,21 @@
 package uniandes.edu.co.proyecto.modelo;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class AfiliadoPK implements Serializable{
     
-    @ManyToOne
-    @JoinColumn(name= "tipoDoc", referencedColumnName = "tipoDoc")
+    //@ManyToOne
+    //@JoinColumn(name= "tipoDoc", referencedColumnName = "tipoDoc")
     private String tipoDoc;
 
-    @ManyToOne
-    @JoinColumn(name = "numDoc", referencedColumnName = "numDoc")
+    //@ManyToOne
+    //@JoinColumn(name = "numDoc", referencedColumnName = "numDoc")
     private Integer numDoc;
 
     public AfiliadoPK(String tipoDoc, Integer numDoc) 
@@ -38,6 +39,19 @@ public class AfiliadoPK implements Serializable{
 
     public void setNumDoc(Integer numDoc) {
         this.numDoc = numDoc;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        AfiliadoPK that = (AfiliadoPK) obj;
+        return Objects.equals(tipoDoc, that.tipoDoc) && Objects.equals(numDoc, that.numDoc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tipoDoc, numDoc);
     }
 
 }
--- src/main/java/uniandes/edu/co/proyecto/modelo/ServicioIpsPK.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/ServicioIpsPK.java
@@ -1,6 +1,7 @@
 package uniandes.edu.co.proyecto.modelo;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
@@ -16,6 +17,9 @@ public class ServicioIpsPK implements Serializable{
     @ManyToOne
     @JoinColumn(name = "nitIps", referencedColumnName = "nit")
     private Ips nitIps;
+
+    public ServicioIpsPK() {
+    }
 
     public ServicioIpsPK(ServicioSalud nombreServicio, Ips nitIps) {
         this.nombreServicio = nombreServicio;
@@ -36,6 +40,20 @@ public class ServicioIpsPK implements Serializable{
 
     public void setNitIps(Ips nitIps) {
         this.nitIps = nitIps;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ServicioIpsPK that = (ServicioIpsPK) obj;
+        return Objects.equals(nombreServicio, that.nombreServicio) &&
+               Objects.equals(nitIps, that.nitIps);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombreServicio, nitIps);
     }
     
 }
