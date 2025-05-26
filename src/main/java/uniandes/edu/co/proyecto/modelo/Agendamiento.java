b9b8b7f691949ac25ea614a88d957592202cafee
Andrea Davila <a.davilab2@uniandes.edu.co>
2025-05-25 21:39:01 -0500

    quitar nonused imports

--- src/main/java/uniandes/edu/co/proyecto/modelo/Agendamiento.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Agendamiento.java
@@ -2,7 +2,6 @@ package uniandes.edu.co.proyecto.modelo;
 
 import java.time.LocalDate;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "agendamientos")
